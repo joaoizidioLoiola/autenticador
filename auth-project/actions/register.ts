df99998dda7d60d21ae0b0b109196391769320ad
joaoizidioLoiola <joaoizidio287@gmail.com>
2024-03-10 11:18:49 -0300

    Adicionado a verificação de email existente no banco de dados, e a encriptação na senha

--- auth-project/actions/register.ts
+++ auth-project/actions/register.ts
@@ -1,17 +1,39 @@
 "use server";
+
 import * as z from "zod";
+import bcrypt from "bcrypt";
+
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
-
-export const register =  async (values: z.infer<typeof RegisterSchema>) => {
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
   const validateFields = RegisterSchema.safeParse(values);
 
-  if( !validateFields.success ) {
+  if (!validateFields.success) {
     return { error: "Campos Inválidos" };
   }
 
-  return { success: "Email enviado!" };
+  const { email, password, name } = validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email já cadastrado" };
+  };
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Enviar token de verificação do email
+
+  return { success: "Usuário criado!" };
 
 };
